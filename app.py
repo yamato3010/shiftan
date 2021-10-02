4ac16b13d56f0876968689fb58e46602b28af21f
DESKTOP-SD0H0H3\yama3 <yama3to10yama3@gmail.com>
2021-10-02 16:55:26 +0900

    ベーシック認証を追加

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
-import numpy as np, pandas as pd
-from pulp import *
+import numpy as np
 import ortoolpy
+import pandas as pd
+from flask import Flask, render_template
+from flask_httpauth import HTTPBasicAuth
+from pulp import *
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+# idとパスワード
+id_list = {"test": "0000"}
+
+#入力されたidに該当するパスワードを
+#比較のために取得する
+@auth.get_password
+def get_pw(id):
+    if id in id_list:
+        return id_list.get(id)
+    return None
 
 @app.route('/')
+@auth.login_required #ここで認証が行われる
 def index():
     return render_template("index.html")
 
