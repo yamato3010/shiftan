f5f37f9d54a61b9055f917028a48291d67d8ed33
koki.f <furukunn@gmail.com>
2021-10-03 19:02:00 +0900

    V_shiftNumberとV_shiftを作成 目的関数作成 ペナルティ定数を定義

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-import ortoolpy
+from ortoolpy import addbinvars
 import pandas as pd
 from flask import Flask, render_template
 from flask_httpauth import HTTPBasicAuth
@@ -7,6 +7,37 @@ from pulp import *
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+
+days = 30 # 提出された表から日数を取得(30は仮) /2忘れない
+member = 4 # 提出された表から人数取得(4は仮)
+
+#ペナルティ定数の定義
+C_needNumber = 10
+C_noAssign =100
+
+#変数の定義
+V_shift = np.array(addbinvars(days * 2, member))
+V_needNumber = np.array(addbinvars(days)) # 0,1を入れれる日数分のリストを作成、後でこのリストに0，1を記入するコードが必要
+# V_noAssign = 
+
+# 問題の定義
+problem = LpProblem(name="penalty", sense=LpMinimize)
+
+# 必要な条件
+# ・×が提出されている人をアサインしてはいけない
+# ・○のみでシフトを作る
+# ・平日、前半は1から2人後半は1人
+# 　休日、前半は3人後半3人
+# ・給料の誤差少ないようにする
+
+# 目的関数
+problem += C_needNumber * lpSum(V_needNumber)
+#    + C_noAssign * lpSum(V_noAssign)
+
+# 制約関数
+for i in range(days*2):
+    problem += V_needNumber >= (lpSum(その日の行のリスト) - 必要人数)
+    problem += V_needNumber >= -(lpSum(その日の行のリスト) - 必要人数)
 
 # ベーシック認証のためのidとパスワード
 # {"ユーザー名": "パスワード"}
